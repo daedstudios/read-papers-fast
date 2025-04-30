6e9d07777d0e68632d181be9af018dcb52cec2c9
eliasheininger <eliasheininger@web.de>
2025-04-30 19:53:57 +0800

    rewamped entire survey UI

--- app/survey/progressBar.tsx
+++ app/survey/progressBar.tsx
@@ -8,9 +8,9 @@ export default function SurveyProgressBar({
   const progress = Math.min((step / total) * 100, 100);
 
   return (
-    <div className="fixed top-0 w-full bg-muted  h-[0.5rem] overflow-hidden mb-4">
+    <div className="fixed top-0 w-full bg-transparent   h-[0.5rem] overflow-hidden mb-4">
       <div
-        className="h-full bg-black transition-all duration-300"
+        className="h-full bg-background transition-all duration-300"
         style={{ width: `${progress}%` }}
       />
     </div>
