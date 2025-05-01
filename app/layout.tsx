14a429fef1c32a593f53a65aebb19549ced674cf
eliasheininger <eliasheininger@web.de>
2025-05-01 16:28:06 +0800

    added buttons for logIn/SignUp

--- app/layout.tsx
+++ app/layout.tsx
@@ -9,6 +9,7 @@ import {
   SignedOut,
   UserButton,
 } from "@clerk/nextjs";
+import { Button } from "@/components/ui/button";
 
 export const metadata: Metadata = {
   title: "ReadPapersFast",
@@ -26,8 +27,16 @@ export default function RootLayout({
         <body className="antialiased">
           <header className="flex absolute top-0 ml-auto w-full justify-end bg-transparent items-center p-4 gap-4 h-16">
             <SignedOut>
-              <SignInButton />
-              <SignUpButton />
+              <SignInButton>
+                <Button className="bg-background/30 w-auto p-4 text-foreground cursor-pointer rounded-[3rem] hover:text-background">
+                  Log In
+                </Button>
+              </SignInButton>
+              <SignUpButton>
+                <Button className="bg-foreground w-auto p-4 text-background cursor-pointer rounded-[3rem] hover:text-foreground hover:bg-background/30">
+                  Sign Up
+                </Button>
+              </SignUpButton>
             </SignedOut>
             <SignedIn>
               <UserButton />
