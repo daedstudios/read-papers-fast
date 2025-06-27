8ad45feed277ccb5c3cbac430a5c85bbcf68e6f3
Akshat Mittal <akshatmittal.0007@gmail.com>
2025-06-27 20:13:35 +0700

    deleting unused code and better structure

--- app/paperG/layout.tsx
+++
@@ -1,9 +0,0 @@
-import { SidebarProvider } from "@/components/ui/sidebar";
-
-export default function RootLayout({
-  children,
-}: Readonly<{
-  children: React.ReactNode;
-}>) {
-  return <SidebarProvider>{children}</SidebarProvider>;
-}
