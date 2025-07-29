5603308cc84f51c6f6a3791f1948cc26450601ff
drifter089 <akshatmittal.0007@gmail.com>
2025-07-29 23:48:56 +0700

    fix pop up

--- components/fact-check-components/CheckoutForm.tsx
+++ components/fact-check-components/CheckoutForm.tsx
@@ -13,21 +13,8 @@ import { X } from "lucide-react";
 
 export default function CheckoutForm({ onClose }: { onClose?: () => void }) {
   return (
-    <div className="fixed inset-0 bg-foreground/30 bg-blur-lg">
-      <Card className="w-full max-w-md bg-white border border-foreground rounded-sm shadow-none relative">
-        <CardHeader>
-          <CardTitle>Subscribe to Base Plan</CardTitle>
-          <CardDescription>
-            Get access to premium features with our base subscription plan.
-          </CardDescription>
-        </CardHeader>
-        <CardContent className="space-y-4">
-          {/* <SignedIn>
-            <CheckoutButton planId="cplan_30Xo9mZqGa9s1xw264aUtpL2J1p" />
-          </SignedIn> */}
-          <PricingTable />
-        </CardContent>
-      </Card>
+    <div className="fixed inset-0 h-screen w-screen p-40">
+      <PricingTable />
     </div>
   );
 }
